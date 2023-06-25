df96f6d7c3d8c4d722f165c0a49091b089777944
jeongeun <wjddms9921@gmail.com>
2023-06-25 18:44:38 +0900

    ADD : 구글 로그인 세팅

--- screens/login.tsx
+++ screens/login.tsx
@@ -1,7 +1,10 @@
-import React from 'react';
-import {Image} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {Image, Pressable} from 'react-native';
 import colors from '../colors';
 import styled from '@emotion/native';
+import {
+  GoogleSignin,
+} from '@react-native-google-signin/google-signin';
 
 const Container = styled.View`
   flex: 1;
@@ -28,6 +31,24 @@ const Button = styled.Pressable`
   justify-content: center;
 `;
 const Login = () => {
+  useEffect(() => {
+    GoogleSignin.configure({
+      webClientId:
+        '1089118701812-h138oegtitdjg555ve33oebtb7tp3ini.apps.googleusercontent.com',
+    });
+  }, []);
+
+  const [token, setToken] = useState<string>();
+
+  const handleClickGoogleBtn = async () => {
+    console.log('--------------------------------');
+    await GoogleSignin.hasPlayServices();
+    const {idToken} = await GoogleSignin.signIn();
+    console.log('idToekn : ', idToken);
+    if (idToken) {
+      setToken(idToken);
+    }
+  };
   return (
     <Container>
       <Image
@@ -42,15 +63,17 @@ const Login = () => {
         당신의 카메라{'\n'} 아름다운 순간을 기록하세요.
       </Text>
       <Button>
-        <Text
-          style={{
-            color: `${colors.white}`,
-            fontSize: 18,
-            fontWeight: 900,
-            lineHeight: 50,
-          }}>
-          시작하기
-        </Text>
+        <Pressable onPress={handleClickGoogleBtn}>
+          <Text
+            style={{
+              color: `${colors.white}`,
+              fontSize: 18,
+              fontWeight: '900',
+              lineHeight: 50,
+            }}>
+            시작하기
+          </Text>
+        </Pressable>
       </Button>
     </Container>
   );
